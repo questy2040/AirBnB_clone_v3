2adf478acefa943004e454452e22f4abd9051588
john <john bosco>
2023-06-21 18:33:43 +0000

    AirBnB V2

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,16 @@
-#!/usr/bin/env python3
-""" Class Amenity """
+#!/usr/bin/python3
+"""This is the amenity class"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
-from models.base_model import BaseModel
 
+class Amenity(BaseModel, Base):
+    """This is the class for Amenity
+    Attributes:
+        name: input name
+    """
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
 
-class Amenity(BaseModel):
-    """ Class Amenity """
-    name = ""
+    # place_amenities = relationship('Place', secondary='place_amenity')
--- models/review.py
+++ models/review.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
-""" review module"""
+"""This is the review class"""
+import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 
-from models.base_model import BaseModel
 
+class Review(BaseModel, Base):
+    """This is the class for Review
+    Attributes:
+        place_id: place id
+        user_id: user id
+        text: review description
+    """
+    __tablename__ = "reviews"
 
-class Review(BaseModel):
-    """ Review model"""
-    place_id = ""
-    user_id = ""
-    text = ""
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    text = Column(String(1024), nullable=False)
